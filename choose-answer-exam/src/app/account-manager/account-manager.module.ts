9dec650aea98ca83068239d0fd63394dcfb6fa84
vosontra-01 <sontra140395@gmail.com>
2020-12-08 09:20:42 +0700

    tra merge code

--- choose-answer-exam/src/app/account-manager/account-manager.module.ts
+++ choose-answer-exam/src/app/account-manager/account-manager.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import {AccountAppRoutingModule, accountRoute} from './account-app-routing.module';
+import {RouterModule} from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+import { MaterialAccountModule } from './material-account.module';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(accountRoute),
+    AccountAppRoutingModule,
+    HttpClientModule,
+    MaterialAccountModule,
   ]
 })
 export class AccountManagerModule { }
