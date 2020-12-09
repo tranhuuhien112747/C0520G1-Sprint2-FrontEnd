07a458d8f83354099a9cd46cafcc8e94d0e7c07e
vosontra-01 <sontra140395@gmail.com>
2020-12-09 17:46:53 +0700

    tra merge code front end

--- choose-answer-exam/src/app/account-manager/component/change-pass/change-pass.component.ts
+++ choose-answer-exam/src/app/account-manager/component/change-pass/change-pass.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {AccountManagerService} from '../../service/account-manager.service';
+import {Router} from '@angular/router';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-change-pass',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-pass.component.css']
 })
 export class ChangePassComponent implements OnInit {
-
-  constructor() { }
+  public formChangePass: FormGroup;
+  public idToFind: number;
+  public errorMessage: string;
+  constructor(
+    private dialogRef: MatDialogRef<ChangePassComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private accountManagerService: AccountManagerService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.formChangePass = this.formBuilder.group( {
+      oldPassword: ['', [Validators.required]],
+      newPassword: ['', [Validators.required, Validators.pattern('^[a-z0-9]{6,30}$')]],
+      confirmPassword: ['', [Validators.required]]
+    }, {validator: comparePassword});
+    this.idToFind = this.data.idUser;
+  }
+  changePass() {
+    this.formChangePass.markAllAsTouched();
+    if (this.formChangePass.valid) {
+      this.accountManagerService.changePassword(this.idToFind, this.formChangePass.value).subscribe(data => {
+        if (data != null){
+          this.errorMessage = data;
+          console.log(data);
+        } else {
+          this.router.navigate(['/infor-account']);
+          this.dialogRef.close();
+        }
+      });
+    }
   }
 
+}
+function comparePassword(c: AbstractControl) {
+  const v = c.value;
+  const isNotEmpty = v.confirmPassword !== '';
+  if (isNotEmpty) {
+    return (v.newPassword === v.confirmPassword) ? null : {
+      passwordNotMatch: true
+    };
+  }
 }
